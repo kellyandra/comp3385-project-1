8e57c59956e9d19af7c4adad9e732c9227cde964
kellyandra <ms.kchristian86@gmail.com>
2024-03-16 07:45:19 -0400

    Create.blade created along with routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-info">
     <div class="container">
-        <a class="navbar-brand" href="#"> <i class="fas fa-home"></i> COMP3385 - Project 1</a>
+        <a class="navbar-brand" href="#"> <i class="fas fa-home"></i> Project 1</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
